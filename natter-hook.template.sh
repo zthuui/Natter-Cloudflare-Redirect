96bc78b511ca9b444ac0820caeccc2aabc3f6faf
与一人同游 <63137661+zthuui@users.noreply.github.com>
2024-04-11 21:27:33 +0800

    Update natter-hook.template.sh

--- natter-hook.template.sh
+++ natter-hook.template.sh
@@ -27,33 +27,33 @@ if [ ${INNER_PORT} == '5000' ]; then
     # So,You must set first rule in here.
     # Such as '/zones/${ZONE}/rulesets/${RULE}/rules' is '>>' to the api list.
     # But '/zones/${ZONE}/rulesets/${RULE}' is '>' to the api list.
-elif [ ${INNER_PORT} == '5678' ]; then
-    PREFIX='admin'
+#elif [ ${INNER_PORT} == '5678' ]; then
+    #PREFIX='admin'
     # IF inner port is 5678,actually domain is admin.example.com
-elif [ ${INNER_PORT} == '55555' ]; then
-    JUMP='1'
+#elif [ ${INNER_PORT} == '55555' ]; then
+    #JUMP='1'
     # qBittorrent. Thanks https://github.com/Mythologyli/qBittorrent-NAT-TCP-Hole-Punching.
-    qb_web_port="8081"
-    qb_username="admin"
-    qb_password="admin"
-    qb_ip="192.168.1.2"
+    #qb_web_port="8081"
+    #qb_username="admin"
+    #qb_password="admin"
+    #qb_ip="192.168.1.2"
     # Update qBittorrent listen port.
-    qb_cookie=$(curl -s -i --header "Referer: $qb_ip:$qb_web_port" --data "username=$qb_username&password=$qb_password" $qb_ip:$qb_web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
-    curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'${PORT}'"}' "$qb_ip:$qb_web_port/api/v2/app/setPreferences"
+    #qb_cookie=$(curl -s -i --header "Referer: $qb_ip:$qb_web_port" --data "username=$qb_username&password=$qb_password" $qb_ip:$qb_web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
+   # curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'${PORT}'"}' "$qb_ip:$qb_web_port/api/v2/app/setPreferences"
     # openwrt Firewall 
     # iptables -t nat -I PREROUTING -i pppoe-wan -p tcp --dport ${INNER_PORT} -j DNAT --to-destination ${qb_ip}:${PORT}
     # OR use uci set Firewall
     # uci set firewall.xxxxxx.dest_port=${PORT}
-    echo "Update qBittorrent listen port to ${PORT}..."
-fi
+  # echo "Update qBittorrent listen port to ${PORT}..."
+#fi
 # Explorer -> ${PREFIX}.Domain == 302 to DDNS_DOMAIN:PORT
 # ${PREFIX}.DOMAIN must be use CloufFlare proxied.
 # Why use natter${5PORT} and ${PREFIX}.Domain? Because Natter has multiple rules, and the rule name / domain name will be same.
 
-if[ ${JUMP} == '0' ]；then
-    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/rulesets/${RULE}/rules" \
-    -H "Authorization: Bearer ${AUTH}" \
-    -H "Content-Type:application/json" \
-    --data "{\"description\": \"natter${INNER_PORT}\",\"expression\": \"(http.host eq \\\"${PREFIX}.${DOMAIN}\\\")\",\"action\": \"redirect\",\"action_parameters\": {\"from_value\": {\"status_code\": 302,\"preserve_query_string\": true,\"target_url\": {\"value\": \"http://${DDNS_DOMAIN}:${PORT}/\"}}},\"enabled\": true}" >/dev/null
-    echo 'ADD '${PREFIX}'.'${DOMAIN}'->'${DDNS_DOMAIN}':'${PORT}' Successful!'
+#if[ ${JUMP} == '0' ]；then
+    #curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/rulesets/${RULE}/rules" \
+    #-H "Authorization: Bearer ${AUTH}" \
+    #-H "Content-Type:application/json" \
+    #--data "{\"description\": \"natter${INNER_PORT}\",\"expression\": \"(http.host eq \\\"${PREFIX}.${DOMAIN}\\\")\",\"action\": \"redirect\",\"action_parameters\": {\"from_value\": {\"status_code\": 302,\"preserve_query_string\": true,\"target_url\": {\"value\": \"http://${DDNS_DOMAIN}:${PORT}/\"}}},\"enabled\": true}" >/dev/null
+    #echo 'ADD '${PREFIX}'.'${DOMAIN}'->'${DDNS_DOMAIN}':'${PORT}' Successful!'
 fi
